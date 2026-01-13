673c05b04c88c2797eb659139b2c7709299e0a48
debarunsengupta <debarunsengupta2512@gmail.com>
2026-01-13 17:38:55 +0530

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/', '/admin/'],
     },
-    sitemap: 'https://polite-mandazi-82b87c.netlify.app/sitemap.xml',
+    sitemap: 'https://salesforcewpocwebsite.netlify.app/sitemap.xml',
   }
 }
 
